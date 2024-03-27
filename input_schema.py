cc07632ccab4ff22bb5f7fb019bb5e17f543c5ff
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-28 01:52:15 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -4,5 +4,11 @@ INPUT_SCHEMA = {
         'required': True,
         'shape': [1],
         'example': ["https://file-examples.com/storage/fe1b802e1565fe057a1d758/2017/10/file_example_JPG_100kB.jpg"]
+    },
+    "question":  {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["Describe this image."]
     }
 }
