4a77c8cfa07d6a33b1015a1b9a404e88e2e53459
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-28 01:08:49 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -1,14 +1,8 @@
 INPUT_SCHEMA = {
-    "prompt": {
+    "image_url": {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["1girl, arima kana, oshi no ko, solo, upper body, v, smile, looking at viewer, outdoors, night"]
-    },
-    "negative_prompt": {
-        'datatype': 'STRING',
-        'required': True,
-        'shape': [1],
-        'example': ["nsfw, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name"]
+        'example': ["https://file-examples.com/storage/fe1b802e1565fe057a1d758/2017/10/file_example_JPG_100kB.jpg"]
     }
 }
