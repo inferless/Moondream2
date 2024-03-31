68fd812c86dadc2f6d9d18a858801123698a7430
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-31 23:44:15 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -3,7 +3,7 @@ INPUT_SCHEMA = {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["https://file-examples.com/storage/fe1b802e1565fe057a1d758/2017/10/file_example_JPG_100kB.jpg"]
+        'example': ["https://github.com/rbgo404/Files/raw/main/moondream.jpg"]
     },
     "question":  {
         'datatype': 'STRING',
